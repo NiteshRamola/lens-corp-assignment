6dd4cddb3f70eb712395feeb3d2ae54960793fcd
Nitesh Ramola <niteshramola90@gmail.com>
2024-08-04 15:29:10 +0530

    updated swagger not to store cache

--- utils/swagger.js
+++ utils/swagger.js
@@ -5,7 +5,21 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = yaml.load(fs.readFileSync('./swagger.yaml', 'utf8'));
 
 function setupSwagger(app) {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  app.use(
+    '/api-docs',
+    (req, res, next) => {
+      res.setHeader(
+        'Cache-Control',
+        'no-store, no-cache, must-revalidate, proxy-revalidate',
+      );
+      res.setHeader('Pragma', 'no-cache');
+      res.setHeader('Expires', '0');
+      res.setHeader('Surrogate-Control', 'no-store');
+      next();
+    },
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument),
+  );
 }
 
 module.exports = setupSwagger;
